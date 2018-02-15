401b38c3f255fb2ae554fbba8efde11d75c401b7
Ajit More <ajit.more@lntinfotech.com>
2018-02-15 19:09:37 +0530

    NodeJS code with typescript is added

---
+++ nodejs_typescript_mongo/build/server.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var logger = require("morgan");
+var cors = require("cors");
+var PostRouter_1 = require("./router/PostRouter");
+var UserRouter_1 = require("./router/UserRouter");
+// Add routes
+// server class
+var Server = /** @class */ (function () {
+    function Server() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    Server.prototype.config = function () {
+        //Set up mongoose
+        var MONGO_URI = 'mongodb://localhost/tes';
+        mongoose.connect(MONGO_URI || process.env.MONGO_URI);
+        // config
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(helmet());
+        this.app.use(logger('dev'));
+        this.app.use(cors());
+    };
+    Server.prototype.routes = function () {
+        var router;
+        router = express.Router();
+        this.app.use('/', router);
+        this.app.use('/api/v1/posts', PostRouter_1.default);
+        this.app.use('/api/v1/users', UserRouter_1.default);
+    };
+    return Server;
+}());
+exports.default = new Server().app;
+//# sourceMappingURL=server.js.map
